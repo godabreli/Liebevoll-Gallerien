6459db916cc23b25445a74d9c37116567dadd518
Godabreli <godabrelidze@hotmail.com>
2025-09-07 22:53:18 +0200

    Image URL Korrektur

--- Frontend/src/util/globalURLS.js
+++ Frontend/src/util/globalURLS.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://liebevollbelichtet.de/';
-export const IMAGE_URL = 'https://liebevollbelichtet.de:5000';
+export const IMAGE_URL = 'https://liebevollbelichtet.de:5000/';
 // export const API_URL = 'http://192.168.1.112:5000/';
